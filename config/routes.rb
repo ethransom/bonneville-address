e41da5a50cb19f04b9197ca10e639daaa852588b
Ethan Ransom <derethanhausen@gmail.com>
2013-01-22 20:55:17 -0700

    Fix sections layout bug

--- config/routes.rb
+++ config/routes.rb
@@ -21,7 +21,7 @@ Blog::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => "posts#index"
+  root :to => "sections#index"
 
   match "page/:page" => "posts#index"
 
